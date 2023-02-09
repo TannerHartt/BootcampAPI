f43bdea3fcef426856a733316b3398fe65446a71
Tanner Hart <tannerhart24@yahoo.com>
2023-02-09 13:52:17 -0800

    Added middleware program to run only when in development mode

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const env = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
+const PORT = process.env.PORT || 5000;
+const morgan = require('morgan');
 
 env.config({ path: './config/config.env' });
 
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
