981f6afedf3b0410db7cc3dfe97be5ac44c35da4
Tanner Hart <tannerhart24@yahoo.com>
2023-02-10 12:53:41 -0800

    Refactored each function to utilize asyncHandler to eliminate the need for try catch blocks

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,92 +1,69 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = async (req, res, next) => {
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
-    try {
-        const bootcamps = await Bootcamp.find();
+    const bootcamps = await Bootcamp.find();
 
-        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+
+});
 
 // @desc    Get a bootcamp by id
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcampById = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
+exports.getBootcampById = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
-        if (!bootcamp) { // If id is correctly formatted but doesn't exist
-            return next(error);
-        }
-
-        res.status(200).json({ success: true, data: bootcamp });
-    } catch (error) {
-        next(error);
+    if (!bootcamp) { // If id is correctly formatted but doesn't exist
+        return next(error);
     }
-}
+
+    res.status(200).json({ success: true, data: bootcamp });
+    
+});
 
 // @desc    Create a new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = async (req, res, next) => {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body);
 
-    try {
-        const bootcamp = await Bootcamp.create(req.body);
-
-        res.status(201).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (error) {
-        next(error);
-    }
-    
-}
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
+});
 
 // @desc    Update a new bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
 
-        if (!bootcamp) { // If id is correctly formatted but doesn't exist
-            return next(error);
-        }
-    } catch (error) {
-        next(error);
+    if (!bootcamp) { // If id is correctly formatted but doesn't exist
+        return next(error);
     }
-    
 
     res.status(200).json({ success: true, data: bootcamp });
-}
+});
 
 // @desc    Delete a bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        if (!bootcamp) { // If id is correctly formatted but doesn't exist
-            return next(error);
-        }
-
-        res.status(200).json({ success: true, data: {} });
-
-    } catch (error) {
-        next(error);
+    if (!bootcamp) { // If id is correctly formatted but doesn't exist
+        return next(error);
     }
-    // 63e6933f9b181bbc43bbd2f0
 
-}
+    res.status(200).json({ success: true, data: {} });
+});
