d4a77b0d805aee86750855ad837e4f1a32b4f700
Tanner Hart <tannerhart24@yahoo.com>
2023-02-10 12:53:03 -0800

    Added error handling for more error types

--- middleware/error.js
+++ middleware/error.js
@@ -12,6 +12,20 @@ function errorHandler(err, req, res, next) {
         error = new ErrorResponse(message, 404);
     }
 
+    // Mongoose duplicate key entered
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message);
+
+        error = new ErrorResponse(message, 400);
+    }
+
     res.status(err.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error'
