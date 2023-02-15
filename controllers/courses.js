635b703cda695604bcf7a7c92dbe35ea0c4bef40
Tanner Hart <tannerhart24@yahoo.com>
2023-02-15 15:29:07 -0800

    Created a delete course function to remove a course from a bootcamp

--- controllers/courses.js
+++ controllers/courses.js
@@ -53,7 +53,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 
 
 // @desc    Add single course
-// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @route   PUT /api/v1/bootcamps/:bootcampId/courses
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId; // Set bootcamp field to bootcampId
@@ -67,6 +67,53 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     }
 
     const course = await Course.create(req.body);
+
+    res.status(200).json({ // Send response
+        success: true,
+        data: course
+    });
+});
+
+
+// @desc    Delete a course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+
+    const course = await Course.findById(req.params.id);
+
+    if (!course) { // If id is correctly formatted but doesn't exist 
+        return next(
+            new ErrorResponse(`No course with id of ${req.params.id}`, 404)
+        );
+    }
+
+    await course.remove();
+
+    res.status(200).json({ // Send response
+        success: true,
+        data: {}
+    });
+});
+
+
+// @desc    Update a course
+// @route   POST /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+
+    let course = await Course.findById(req.params.id);
+
+    if (!course) { // If id is correctly formatted but doesn't exist 
+        return next(
+            new ErrorResponse(`No course with id of ${req.params.id}`, 404)
+        );
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
 
     res.status(200).json({ // Send response
         success: true,
