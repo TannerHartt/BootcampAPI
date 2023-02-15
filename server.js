257c82a70a0d3d2697590a58a042523b66eff5d8
Tanner Hart <tannerhart24@yahoo.com>
2023-02-15 13:44:53 -0800

    Included the course handlers to the server file.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const env = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 const PORT = process.env.PORT || 5000;
 const morgan = require('morgan');
 const colors = require('colors');
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 app.use(errorHandler);
 
 const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
