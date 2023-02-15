9738b8d932f862117127d133249698018091d6b7
Tanner Hart <tannerhart24@yahoo.com>
2023-02-15 13:43:29 -0800

    Created a route controller file to handle all course related routes.

---
+++ controllers/courses.js
@@ -0,0 +1,26 @@
+const Course = require('../models/Course');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+
+// @desc    Get all courses
+// @route   GET /api/v1/courses
+// @route   GET /api/v1/bootcamps/:bootcampId/courses
+// @access  Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+    let query;
+
+    if (req.params.bootcampId) { // If bootcampId is specified, find all courses that belong to that bootcamp
+        query = Course.find({ bootcamp: req.params.bootcampId });
+    } else { // If bootcampId is not specified, find all courses
+        query = Course.find();
+    }
+
+    const courses = await query; // Execute query
+
+    res.status(200).json({ // Send response
+        success: true,
+        count: courses.length,
+        data: courses
+    });
+});
