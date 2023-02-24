ed282688952e14013ca9e2ca5110b990e2587e0b
Tanner Hart <tannerhart24@yahoo.com>
2023-02-24 11:14:55 -0800

    Fixed model error and added password encryption using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -18,19 +19,26 @@ const UserSchema = new mongoose.Schema({
         type: String,
         enum: ['user', 'publisher'],
         default: 'user',
-        password: {
-            type: String,
-            required: [true, 'Please add a password'],
-            minlength: 6,
-            select: false,
-        },
-        resetPasswordToken: String,
-        resetPasswordExpire: Date,
-        createdAt: {
-            type: Date,
-            default: Date.now
-        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 
 module.exports = mongoose.model('User', UserSchema);
