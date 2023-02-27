92f0a65c7dfc5ef398eabce50c8743d8f9248fdf
Tanner Hart <tannerhart24@yahoo.com>
2023-02-27 11:43:42 -0800

    Included new authorize function to all protected routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,7 +18,7 @@ const advancedResults = require('../middleware/advancedResults');
 // Include other resource routers
 const courseRouter = require('./courses');
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
@@ -27,16 +27,16 @@ router.route('/radius/:zipcode/:distance')
     .get(getbootcampsWithinRadius);
 
 router.route('/:id/photo')
-    .put(protect, bootcampPhotoUpload);
+    .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router.route('/')
     .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(protect, createBootcamp);
+    .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 
 router.route('/:id')
     .get(getBootcampById)
-    .put(protect, updateBootcamp)
-    .delete(protect, deleteBootcamp);
+    .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
