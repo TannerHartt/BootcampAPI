382aa78f41196bde9dd3b3c48dee7b0b22487d32
Tanner Hart <tannerhart24@yahoo.com>
2023-02-28 15:00:15 -0800

    Added rate limiter, http params polution prevention, and cors to the server

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -47,6 +50,20 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors()); // Allow all origins to access the API
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
