2c447c02c2f4d2f97a14d66bf52d9dafb0469706
Tanner Hart <tannerhart24@yahoo.com>
2023-02-27 17:29:42 -0800

    Added a route to get a single review by id

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,12 +5,17 @@ const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 const {
     getReviews,
+    getReview
 } = require('../controllers/reviews');
 
 
 
 router.route('/')
     .get(advancedResults(Review, { path: 'bootcamp', select: 'name description' }), getReviews);
+
+
+router.route('/:id')
+    .get(getReview)
 
 
 module.exports = router;
