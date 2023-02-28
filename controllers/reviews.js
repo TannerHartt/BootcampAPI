7e6c80dec11de359106a36253e4996760ff077ff
Tanner Hart <tannerhart24@yahoo.com>
2023-02-27 23:00:17 -0800

    Created functions to update and delete a review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -66,3 +66,55 @@ exports.addReview = asyncHandler(async (req, res, next) => {
         data: review
     });
 });
+
+
+// @desc    Update a review
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+
+    let review = await Review.findById(req.params.id); // Find the review
+
+    // If no review is found, return error
+    if (!review) return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404));
+
+    // Make sure review belongs to user or user is an admin
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this review`, 401));
+    }
+
+    review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    // Review was found, return it
+    res.status(200).json({ // Send response
+        success: true,
+        data: review
+    });
+});
+
+
+// @desc    Delete a review
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id); // Find the review
+
+    // If no review is found, return error
+    if (!review) return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404));
+
+    // Make sure review belongs to user or user is an admin
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this review`, 401));
+    }
+
+    await review.remove(); // Remove the review
+
+    // Review was found, return it
+    res.status(200).json({ // Send response
+        success: true,
+        data: {}
+    });
+});
