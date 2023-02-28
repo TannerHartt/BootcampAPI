2bb9ff007b0248a907f45adb55a9c2507e8b3d57
Tanner Hart <tannerhart24@yahoo.com>
2023-02-27 22:59:59 -0800

    Created static function to calculate avg rating after save and before delete

--- models/Review.js
+++ models/Review.js
@@ -36,4 +36,38 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true }); // This makes it so that a user can only add one review per bootcamp
 
+// Static method to get avg rating
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+    const obj = await this.aggregate([ // Aggregate is a mongoose method that allows us to do advanced queries
+        {
+            $match: { bootcamp: bootcampId } // Match bootcampId to the bootcamp field
+        },
+        {
+            $group: { // Group by bootcamp
+                _id: '$bootcamp', 
+                averageRating: { $avg: '$rating' } // Calculate average tuition and set the averageCost field to the result
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, { // Update the bootcamp with the new averageCost
+            averageRating: obj[0].averageRating
+        });
+    } catch (err) {
+        console.error(err); // Log error
+    }
+}
+
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function() {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function() {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
